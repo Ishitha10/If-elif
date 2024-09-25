310d2bdf3cec03f13f371090086a559dddd2aea9
Ishitha10 <ishithabommu1@gmail.com>
2024-09-25 20:26:03 +0100

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,21 @@
+a = "Hello"
+b = "Hi"
+if a!=b:
+    print("Both are different")
+else:
+    print("They are both equal")
+
+x = 10
+y = 50
+if x <= 10 and y > 30:
+    print("Both of the conditions are met")
+else:
+    print("The conditions are not met")
+
+n = 20
+m = 30
+if n <= 20 or m > 30:
+    print("One of the conditions are met")
+else:
+    print("Both conditions are not met")
+    
